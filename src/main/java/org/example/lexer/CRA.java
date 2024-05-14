a8d74e14a8b6a36e94410fcd0e7d3fa1a13d1d4d
anastasizzz <anastasizzzs10@gmail.com>
2024-05-14 21:28:15 +0300

    upd types of token, added exception handler functions

--- src/main/java/org/example/lexer/CRA.java
+++ src/main/java/org/example/lexer/CRA.java
@@ -3,11 +3,14 @@ package org.example.lexer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class CRA {
+
+    static boolean hasError = false;
     public static void main(String[] args) throws IOException {
         if(args.length >1) {
             System.out.println("Usage: cra");
@@ -17,9 +20,15 @@ public class CRA {
         }
     }
 
+
+
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
+        if (hasError) {
+            System.exit(65);
+        }
     }
 
     private static void runPromt() throws IOException {
@@ -32,6 +41,7 @@ public class CRA {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hasError = false;
         }
     }
 
@@ -39,5 +49,21 @@ public class CRA {
     private static void run(String line) {
         org.example.lexer.Scanner scanner = new Scanner(line);
         List<Token> tokens = scanner.getTokens();
+    }
+
+
+    static void report(
+            int line,
+            String msg,
+            String where
+    ) {
+        System.out.println("[line " + line + "] Error" + where + ": " + msg);
+        hasError = true;
+    }
+    static void error(
+            int line,
+            String msg
+    ) {
+        report(line, "", msg);
     }
 }
